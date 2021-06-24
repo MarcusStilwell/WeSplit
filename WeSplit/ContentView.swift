64e7e34e426c5d84560303542a14071f5373dcdc
MarcusStilwell <marcusstilwell@icloud.com>
2021-06-24 16:17:04 -0700

    Added calculation for total price including tip.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,22 +12,32 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 2
     let tipPercentages = [10, 15, 20, 25, 0]
+    var totalPerPerson: Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationView{
         Form {
             Section {
                 TextField("Amount", text: $checkAmount)
                     .keyboardType(.decimalPad)
+                
                 Picker("Number of people", selection: $numberOfPeople){
                     ForEach(2 ..< 100){
                         Text("\($0) people")
                     }
                 }
             }
-        }.navigationBarTitle("WeSplit")
-            Section{
-                Text("$\(checkAmount)")
-            }
+        
             Section(header: Text("How much tip do you want to leave?")){
                 Picker("Tip percentage", selection: $tipPercentage) {
                     ForEach(0 ..< tipPercentages.count){
@@ -36,7 +46,12 @@ struct ContentView: View {
                 }
                 .pickerStyle(SegmentedPickerStyle())
             }
+            Section{
+                Text("$\(totalPerPerson, specifier: "%.2f")")
+            }
         }
+        }
+        .navigationBarTitle("WeSplit")
     }
 }
 
